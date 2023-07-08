aa4461fd2fb606309ee98d010e45871337692962
abrielleperry <abrielleperry22@icloud.com>
2023-07-08 15:52:20 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+typedef struct form
+{
+	char type;
+	int (*f)(va_list);
+} type_f;
+
+/* Prototypes */
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_string(va_list varg);
+int print_char(va_list varg);
+int print_integer(va_list varg);
+int print_edge(char e);
+
+#endif
